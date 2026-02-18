2ee6ee27a634fcfc62a89b461170e5f0dc2e963d
Jimmy Laroche <jimmy.lar91@gmail.com>
2026-02-18 13:57:45 -0700

    Add period after my name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return(
         <>
             <hr />
-            <footer className="footer"><small>©{today.getFullYear()} Recipe Generator App by Jimmy Laroche</small></footer>
+            <footer className="footer"><small>©{today.getFullYear()} Recipe Generator App by Jimmy Laroche.</small></footer>
         </>
     )
 }
